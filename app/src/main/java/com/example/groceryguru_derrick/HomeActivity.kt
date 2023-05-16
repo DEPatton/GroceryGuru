2a61a45be718534695e851fa805f694acf6dda86
Derrick Patton <DEPatton@student.fullsail.edu>
2023-05-16 15:24:48 -0400

    menu dropdown finished

--- app/src/main/java/com/example/groceryguru_derrick/HomeActivity.kt
+++ app/src/main/java/com/example/groceryguru_derrick/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.groceryguru_derrick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
-import java.util.jar.Attributes.Name
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var menuimagebutton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -21,9 +26,32 @@ class HomeActivity : AppCompatActivity() {
         }
         else
         {
-
             welcomeText.text = "Welcome! Please Sign In."
-
         }
     }
+        fun onClick(view:View)
+        {
+            // Referencing and Initializing the button
+            menuimagebutton = findViewById(R.id.clickBtn)
+
+            // Setting onClick behavior to the button
+            menuimagebutton.setOnClickListener {
+                // Initializing the popup menu and giving the reference as current context
+                val popupMenu = PopupMenu(this@HomeActivity, menuimagebutton)
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.menuInflater.inflate(R.menu.dropdown_menu, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+                    // Toast message on menu item clicked
+                    Toast.makeText(
+                        this@HomeActivity,
+                        "You Clicked " + menuItem.title,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+                // Showing the popup menu
+                popupMenu.show()
+            }
+        }
 }
