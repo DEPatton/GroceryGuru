3a844b649701bc5b6f2203405caf72037a6c55bb
Arianna Hernandez <AAHernandez2@student.fullsail.edu>
2023-05-18 18:02:45 -0500

    Bottom navigation and fragments added for app pages

--- app/src/main/java/com/example/groceryguru_derrick/MainActivity.kt
+++ app/src/main/java/com/example/groceryguru_derrick/MainActivity.kt
@@ -1,46 +1,38 @@
 package com.example.groceryguru_derrick
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.groceryguru_derrick.ui.theme.GroceryGuruDerrickTheme
+import androidx.fragment.app.Fragment
+import com.example.groceryguru_derrick.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GroceryGuruDerrickTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_home -> replaceFragment(Home())
+                R.id.navigation_profile -> replaceFragment(Profile())
+                R.id.navigation_grocery -> replaceFragment(Grocery())
             }
+            true
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GroceryGuruDerrickTheme {
-        Greeting("Android")
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
+
+
+
+
