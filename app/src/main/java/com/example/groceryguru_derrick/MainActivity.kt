c6b1bb2dc14badb59ed2873ea0e28d213181326f
SAlexander21 <sealexander1@student.fullsail.edu>
2023-05-25 16:29:48 -0400

    Fixed problems with merging process.

--- app/src/main/java/com/example/groceryguru_derrick/MainActivity.kt
+++ app/src/main/java/com/example/groceryguru_derrick/MainActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.groceryguru_derrick.databinding.ActivityMainBinding
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
 
