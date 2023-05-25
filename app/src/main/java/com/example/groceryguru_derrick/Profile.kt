9ddc1b152e4aab2fd9b2eadba8a17a18323e15cd
Derrick Patton <DEPatton@student.fullsail.edu>
2023-05-25 17:52:24 -0400

    Added profile to the home page

--- app/src/main/java/com/example/groceryguru_derrick/Profile.kt
+++ app/src/main/java/com/example/groceryguru_derrick/Profile.kt
@@ -31,7 +31,7 @@ class Profile : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.profile_layout, container, false)
     }
 
     companion object {
